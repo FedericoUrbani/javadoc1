b303cb8e784aa8e8db1cc146b3625337e74239fe
Federico Urbani <urbanifederico5@gmail.com>
2023-01-24 22:00:58 +0100

    wordgames

---
+++ javadoc01/src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args) {
+        WordGames a= new WordGames();
+        System.out.println(  a.addHelloWord("marco"));
+        System.out.println( a.getFullName("marco","materazzi"));
+    }
+}
---
+++ javadoc01/src/WordGames.java
@@ -0,0 +1,11 @@
+public class WordGames {
+
+    public String addHelloWord(String word){
+        String b="Hello "+word;
+        return b;
+    }
+    public String getFullName(String name,String surname){
+        String b=name.concat(surname);
+        return b;
+    }
+}
